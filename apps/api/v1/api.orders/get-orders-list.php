7b1832dd30114979ca9acc74f97c8596938b3da1
pradeepkarn <mail2pkarn@gmail.com>
2023-10-23 18:23:53 +0530

    gfas

--- apps/api/v1/api.orders/get-orders-list.php
+++ apps/api/v1/api.orders/get-orders-list.php
@@ -1,17 +1,19 @@
 <?php 
 try {
-    extract(apache_request_headers());
-    if (isset($API_KEY) && hash_equals(sitekey, $API_KEY)) {
-        $orders = new Order_api;
+    $orders = new Order_api;
         $orders->get_orders();
-    }else{
-        $_SESSION['msg'][] = "API KEY IS INVALID";
-        $api['success'] = false;
-        $api['data'] = null;
-        $api['msg'] = msg_ssn(return: true);
-        echo json_encode($api);
-        exit;
-    }
+    // extract(apache_request_headers());
+    // if (isset($API_KEY) && hash_equals(sitekey, $API_KEY)) {
+    //     $orders = new Order_api;
+    //     $orders->get_orders();
+    // }else{
+    //     $_SESSION['msg'][] = "API KEY IS INVALID";
+    //     $api['success'] = false;
+    //     $api['data'] = null;
+    //     $api['msg'] = msg_ssn(return: true);
+    //     echo json_encode($api);
+    //     exit;
+    // }
 } catch (\Throwable $th) {
         $_SESSION['msg'][] = "Something went wrong";
         $api['success'] = false;
